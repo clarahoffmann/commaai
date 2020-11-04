b8b83c2f4ae23021b48d96efff8939666cf0be87
clarahoffmann <claracharlotteh@web.de>
2020-11-04 14:44:03 +0000

    new derivatives for ridge VA

--- 04b_VA/helpers.py
+++ 04b_VA/helpers.py
@@ -8,76 +8,66 @@ def generate_z(mean_epsilon,var_epsilon ):
 def generate_epsilon(mean_z, var_z):
     return np.random.multivariate_normal(mean_z,var_z, 1).T
 
-# gradient w.r.t. lambda_j
-def grad_theta_h_lambda_j(Lambda, S, beta, B_zeta, z, tau,j):
-    s_diag = np.diag(S)
-    psi_j = B_zeta[:,j]
-    lambda_j_sq = (np.diag(Lambda)[j])**2
-    A = np.diag((B_zeta[:,j]**2)*lambda_j_sq)
-    return(- (1/2)*lambda_j_sq*sum((psi_j**2)*(z**2))
-           + (1/2)*lambda_j_sq*sum((psi_j**2)*(s_diag**2))
-           + (1/2)*multi_dot([beta.T, B_zeta.T, A, S, z])
-           + (beta[j]**2)/(2*lambda_j_sq)
-           - (lambda_j_sq/(tau**2))/(1+lambda_j_sq/(tau**2)) + 1/2)
+def delta_beta(z, u, B, S, beta, tBB):
+    return((z*(1/S)).dot(B) - beta.T.dot(tBB) - beta/np.exp(u))
 
-# gradient w.r.t. beta
-def delta_theta_h_beta(z, S, B_zeta, Lambda, beta):
-    return (z.T).dot(np.linalg.inv(S)).dot(B_zeta) - 2*(beta.T).dot(B_zeta.T).dot(B_zeta) + ((np.linalg.inv(Lambda**2))).dot(beta)
+def delta_tau(u, B, S2, dS2, z, beta, theta, betaBt):
+    p = B.shape[1]
+    return( - 0.5*np.sum(dS2/S2) 
+            - 0.5*np.sum((z**2)*(-dS2/(S2**2)))
+            + np.sum(betaBt*((-0.5*dS2/(S2**1.5))*z)) 
+            - (0.5*p - 0.5)
+            + 0.5*(beta.T.dot(beta))/np.exp(u) 
+            - 0.5*(np.exp(u)/theta)**0.5)
 
-# gradient w.r.t. tau
-def delta_tau(p, Lambda, tau):
-    tau_sq = tau**2
-    return -(p-1) + 2*sum([((Lambda[i,i])**2/tau_sq)*((1+((Lambda[i,i])**2/tau_sq))**(-1)) for i in range(0,Lambda.shape[0])]) - 2*(tau_sq/(1+tau_sq)) 
+def compdS(tau2, W):
+    tildeW = tau2*W
+    S2 = 1/(1+tildeW)
+    dS2 = -tildeW/((1+tildeW)**2)
+    ddS2 = -tildeW/((1+tildeW)**2) + 2*(tildeW**2)/((1+tildeW)**3)
+    
+    S = np.sqrt(S2)
+    return(S2, dS2, ddS2, S)
 
-def Delta_theta(vartheta_t, B_zeta, n, z, p):
+def Delta_theta(vartheta_t, B, n, z, p, tBB, betaBt, theta, beta, W):
     vartheta_new = vartheta_t.copy()
     beta_t = vartheta_new[0:p].reshape(p,)
-    lambda_js_t = np.exp(vartheta_new[p:2*p])
-    tau_t = np.exp(vartheta_new[2*p])
+    u = vartheta_new[p]
     
-    # Update values that are dependent on t
-    # update lambda with lambda_js
-    Lambda_t = np.diag(lambda_js_t.T[0])
-
-    # update S for lambda values
-    S_t = np.diag([(1 + ((B_zeta[i,:].T).dot(Lambda_t**2)).dot(B_zeta[i,:]))**(-1/2) for i in range(0,n)])
+    S2, dS2, ddS2, S = compdS(np.exp(u), W)
     
     # Gradient w.r.t. beta
-    grad_beta = delta_theta_h_beta(z, S_t, B_zeta, Lambda_t, beta_t)
-    
-    # Gradient w.r.t. lambda_js
-    grad_lambda_js = [(grad_theta_h_lambda_j(Lambda_t, S_t, beta_t, B_zeta, z, tau_t,j_i)) 
-                      for j_i in range(0,Lambda_t.shape[0])]
-    grad_lambda_js = np.array(grad_lambda_js).T
+    grad_beta = delta_beta(z, u, B, S, beta, tBB)
     
     # Gradient w.r.t. tau
-    grad_tau = delta_tau(p, Lambda_t, tau_t)
+    grad_tau = delta_tau(u, B, S2, dS2, z, beta, theta, betaBt).item()
     
-    return(np.append(grad_beta, np.append(grad_lambda_js, grad_tau)))
+    return(np.append(grad_beta, grad_tau))
 
 def Delta_mu(gradient_h_t, BBD_inv, z_t, d_t, epsilon_t, B_t):
     return gradient_h_t 
 
 def Delta_B(B_zeta,n,z, p, B_t, gradient_h_t, z_t,D_t, d_t, epsilon_t, BBD_inv):
-    gradient_h = (gradient_h_t.reshape(2*p+1,1)).copy()
+    gradient_h = (gradient_h_t.reshape(p+1,1)).copy()
     return( gradient_h.dot(z_t.T) 
            + BBD_inv.dot(B_t.dot(z_t) 
            + (d_t*epsilon_t)).dot(z_t.T))
 
 def Delta_D(gradient_h_t, epsilon_t, D_t, d_t,p, BBD_inv):
-    gradient_h_diag = np.diag((gradient_h_t.reshape(2*p+1))).copy()
+    gradient_h_diag = np.diag((gradient_h_t.reshape(p+1))).copy()
     return(
     gradient_h_diag.dot(epsilon_t)) 
     + (multi_dot([BBD_inv, (B.dot(z)  + (d_t*epsilon_t)), epsilon]))
     
 
-def log_density(z, beta, B_zeta, Lambda, p, tau, n):   
-    S =  np.diag([(1 + ((B_zeta[i,:].T).dot(Lambda**2)).dot(B_zeta[i,:]))**(-1/2) for i in range(0,n)])
+def log_density(z, u,  beta, B, Lambda, p,  n, S, S2, tBB, theta, betaBt):   
     return (
-          - (1/2)*sum(np.log(np.diag(S**2))) - multi_dot([(1/2)*(z - multi_dot([S, B_zeta, beta])).T, np.linalg.inv(S.dot(S)), (z - multi_dot([S, B_zeta, beta]))])
-        +(1/2)*sum(np.log(np.diag(Lambda**2))) - (1/2)*sum([beta[i]**2/(Lambda[i,i]**2) for i in range(0,p)])
-        -(p-1)*math.log(tau) - sum(np.log([1+(Lambda[i,i]**2)/(tau**2) for i in range(0,Lambda.shape[0])])) - (p-1)*math.log(1+tau**2) 
-    )
+          - 0.5*sum(np.log(S2))
+          - 0.5*z.dot((1/S2)*z) + betaBt.dot(z*(1/S)) 
+          - 0.5*beta.T.dot(tBB).dot(beta) 
+          - 0.5/np.exp(u)*np.sum(beta**2) 
+          - 0.5*u*(p-1) 
+          - np.sqrt(np.exp(u)/theta))
 
 def multivariate_normal(x, d, mean, covariance):
     """pdf of the multivariate normal distribution."""
